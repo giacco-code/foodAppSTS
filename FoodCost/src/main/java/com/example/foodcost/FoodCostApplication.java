567284028e9ef76cf60bddc730cef5b21c6a214d
Giacco <giachix97@gmail.com>
2023-03-04 15:28:24 +0100

    first commit

---
+++ FoodCost/src/main/java/com/example/foodcost/FoodCostApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodcost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCostApplication.class, args);
+	}
+
+}
